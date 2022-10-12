29aeeabb732999833e558c2f835f5e1430a226e5
Curro Garcia <curro-91@hotmail.com>
2022-10-12 20:41:07 +0200

    bonus 6 funcionando, alomejor falta añadir alguna info de las ccervezas

--- app.js
+++ app.js
@@ -51,6 +51,19 @@ app.get("/random-beer", (req, res) => {
   .catch(error => console.log(error))
 })
 
+app.get("/beers/:id", (req, res) => {
+  console.log("entrando en iteraccion 6")
+  let {id} = req.params
+  punkAPI.getBeer(id)
+  .then(beersFromApi => {
+    console.log(beersFromApi)
+    res.render("all-beers.hbs", {
+      beersFromApi
+    })
+  })
+  .catch(error => console.log(error))
+})
+
 
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
